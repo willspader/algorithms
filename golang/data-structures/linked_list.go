98f67ec9655be8c58a24bc0732764ba5831b15fe
William Cabral Donato Spader <spaderwilliam@hotmail.com>
2022-11-04 11:56:00 -0300

    insertAt and removeAt implementation

--- golang/data-structures/linked_list.go
+++ golang/data-structures/linked_list.go
@@ -15,35 +15,99 @@ type node struct {
 }
 
 func main() {
-	// testing purposes
-	// expect [5, 9, 7, -3, 2]
-
 	var linkedList linkedList = linkedList{head: nil, length: 0}
 
-	linkedList.add(5, 0)
-	linkedList.add(7, 1)
-	linkedList.add(2, 2)
-	linkedList.add(9, 1)
-	linkedList.add(-3, 3)
+	linkedList.insertAt(5, 0)
+	linkedList.insertAt(7, 1)
+	linkedList.insertAt(2, 2)
+	linkedList.insertAt(9, 1)
+	linkedList.insertAt(-3, 3)
 
+	// expect [5, 9, 7, -3, 2]
 	fmt.Println(linkedList)
 	fmt.Println(linkedList.head.data)
 	fmt.Println(linkedList.head.next.data)
 	fmt.Println(linkedList.head.next.next.data)
 	fmt.Println(linkedList.head.next.next.next.data)
 	fmt.Println(linkedList.head.next.next.next.next.data)
+
+	linkedList.removeAt(0)
+	linkedList.removeAt(3)
+	linkedList.removeAt(1)
+
+	// expect [9, -3]
+	fmt.Println(linkedList)
+	fmt.Println(linkedList.head.data)
+	fmt.Println(linkedList.head.next.data)
+
+	linkedList.insertAt(0, 1)
+	linkedList.insertAt(7, 3)
+	linkedList.insertAt(4, 4)
+	linkedList.insertAt(2, 0)
+
+	// expect [2, 9, 0, -3, 7, 4]
+	fmt.Println(linkedList)
+	fmt.Println(linkedList.head.data)
+	fmt.Println(linkedList.head.next.data)
+	fmt.Println(linkedList.head.next.next.data)
+	fmt.Println(linkedList.head.next.next.next.data)
+	fmt.Println(linkedList.head.next.next.next.next.data)
+	fmt.Println(linkedList.head.next.next.next.next.next.data)
 }
 
-func (list *linkedList) add(data int, pos int) {
+func (list *linkedList) insertAt(data int, pos int) {
 	if list.length == 0 {
-		var newNode node = node{next: nil, data: data}
-		list.head = &newNode
+		list.head = &node{next: nil, data: data}
 		list.length++
 		return
 	}
 
-	if pos > list.length {
+	if pos == 0 {
+		list.head = &node{next: list.head, data: data}
+		list.length++
 		return
+	}
+
+	var prev *node = list.getAt(pos)
+
+	if prev != nil {
+		prev.next = &node{next: prev.next, data: data}
+		list.length++
+	}
+}
+
+func (list *linkedList) removeAt(pos int) {
+	if list.length == 0 {
+		return
+	}
+
+	if pos == 0 {
+		if list.length == 1 {
+			list.head = nil
+		} else {
+			list.head = list.head.next
+		}
+		list.length--
+		return
+	}
+
+	var prev *node = list.getAt(pos)
+
+	if prev == nil {
+		return
+	}
+
+	if pos == list.length-1 {
+		prev.next = nil
+	} else {
+		prev.next = prev.next.next
+	}
+	list.length--
+}
+
+func (list *linkedList) getAt(pos int) *node {
+	if pos > list.length {
+		return nil
 	}
 
 	var head *node = list.head
@@ -54,14 +118,5 @@ func (list *linkedList) add(data int, pos int) {
 		idx++
 	}
 
-	var newNode node
-	if head.next != nil {
-		newNode = node{next: head.next, data: data}
-	} else {
-		newNode = node{next: nil, data: data}
-	}
-
-	head.next = &newNode
-	list.length++
-	return
+	return head
 }
