9664a0443b59b4f35c7b4f467c2444a5613f89d4
William Spader <36180147+willspader@users.noreply.github.com>
2022-12-05 21:04:31 -0300

    Delete python directory

--- python/leetcode/remove-duplicates-sorted-array.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def removeDuplicates(self, nums):
-        i = 1
-        for j in range(len(nums) - 1):
-            if nums[j+1] > nums[j]:
-                nums[i] = nums[j+1]
-                i += 1
-
-        return i
--- python/leetcode/watermelon-4a-codeforces.py
+++
@@ -1,5 +0,0 @@
-if __name__ == '__main__':
-    n = int(input())
-
-    response = "YES" if n > 2 and n % 2 == 0 else "NO"
-    print(response)
